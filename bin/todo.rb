4d6486370c14e9e5af612384e94289e8eae5021d
Emma Buchheim <emma.buchheim@gmail.com>
2017-07-16 15:06:02 -0500

    Add Menu module.

--- bin/todo.rb
+++ bin/todo.rb
@@ -4,11 +4,14 @@ require_relative "../lib/classes.rb"
 require_relative "../lib/modules.rb"
 
 if __FILE__ == $PROGRAM_NAME
-  my_list = List.new
+  #my_list = List.new
 
-  my_first_task = Task.new("Snuggles with Tyler!")
-  my_list.add(my_first_task)
-  my_second_task = Task.new ("Snuggles with puppies!")
-  my_list.add(my_second_task)
-  my_list.show
+  #my_first_task = Task.new("Snuggles with Tyler!")
+  #my_list.add(my_first_task)
+  #my_second_task = Task.new ("Snuggles with puppies!")
+  #my_list.add(my_second_task)
+  #my_list.show
+  include Menu
 end
+
+Menu.show
--- lib/modules.rb
+++ lib/modules.rb
@@ -1,2 +1,14 @@
 module Menu
+
+  def menu
+    puts "Welcome to your To Do List!
+    \nThis shortcut menu will help you use the system:
+    \n1 = Add
+    \n2 = show
+    \nq = Quit"
+  end
+
+  def show
+    menu
+  end
 end
