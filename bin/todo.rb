b3b431934719f5f372fba5f0a08e98c1063630e5
Emma Buchheim <emma.buchheim@gmail.com>
2017-07-16 09:13:51 -0500

    Add List class and link .rbs

--- bin/todo.rb
+++ bin/todo.rb
@@ -1,1 +1,5 @@
 #This is where I'm keeping my ACTIONS!
+require "../lib/classes.rb"
+
+my_list = List.new
+puts my_list
--- lib/classes.rb
+++ lib/classes.rb
@@ -1,6 +1,4 @@
 #This is where I'm keeping my CLASSES!
 class List
-  def initialize(task)
-    @task = task
-  end
+
 end
