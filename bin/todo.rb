24f2574a58bd089556a7dd96ba20012dd89a2e1f
Emma Buchheim <emma.buchheim@gmail.com>
2017-07-16 19:37:58 -0500

    Minor code cleanup.

--- bin/todo.rb
+++ bin/todo.rb
@@ -8,32 +8,33 @@ if __FILE__ == $PROGRAM_NAME
   include Promptable
 
   my_list = List.new
-  Menu.show
-  input = Promptable.prompt
+  show
+  input = prompt
   until input == "q"
     case input
       when "1"
-        task_input = Promptable.prompt("What would you like to add?")
+        task_input = prompt("What would you like to add?")
         my_list.add(Task.new(task_input))
-        input = Promptable.prompt("\nWould you like to add another task? (Y/n)")
+        input = prompt("\nWould you like to add another task? (Y/n)")
         if input.upcase == "Y" || input.upcase == "YES"
           input = "1"
         else
-          input = Promptable.prompt
+          input = prompt
         end
       when "2"
         my_list.show
-        input = Promptable.prompt
+        input = prompt
       when "3"
-        filename_input = Promptable.prompt("\nPlease enter the name of the file you would like to write to:")
+        filename_input = prompt("\nPlease enter the name of the file you would like to write to:")
         my_list.write_to_file(filename_input)
-        input = Promptable.prompt
+        input = prompt
       when "4"
-        filename_input = Promptable.prompt("\nPlease enter the name of the file you would like to read from:")
+        filename_input = prompt("\nPlease enter the name of the file you would like to read from:")
         my_list.read_to_file(filename_input)
-        input = Promptable.prompt
+        input = prompt
       else
-        input = Promptable.prompt
+        puts "Sorry, I don't recognize that input..."
+        input = prompt
     end
   end
   puts "Aw, I guess you'll go be productive now.  Bye!"
